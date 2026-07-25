78d0f92bcc66b430629b5f97e1a464fe08d4eaba
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-25 18:47:13 +0530

    Update billing.js

--- pos/js/billing.js
+++ pos/js/billing.js
@@ -625,19 +625,14 @@ function scrollToCart(){
 function toggleSummary(){
 
     const content = document.getElementById("summaryContent");
-
     const arrow = document.getElementById("summaryArrow");
 
     content.classList.toggle("hide");
 
     if(content.classList.contains("hide")){
-
         arrow.innerHTML = "▼";
-
     }else{
-
         arrow.innerHTML = "▲";
-
     }
 
 }
