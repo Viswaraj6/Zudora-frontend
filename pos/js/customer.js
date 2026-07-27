b3b555fdffae15699dc995d1a6f5ac1f233c52e1
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-27 20:49:55 +0530

    Update customer.js

--- pos/js/customer.js
+++ pos/js/customer.js
@@ -82,3 +82,69 @@ function selectCustomer(id){
     window.location.href = "billing.html";
 
 }
+function openAddCustomer(){
+
+    document
+        .getElementById("customerPopup")
+        .classList.remove("hidden");
+
+}
+
+function closeCustomerPopup(){
+
+    document
+        .getElementById("customerPopup")
+        .classList.add("hidden");
+
+}
+async function saveCustomer(){
+
+    const customer = {
+
+        name: document.getElementById("custName").value.trim(),
+
+        mobile: document.getElementById("custMobile").value.trim(),
+
+        email: document.getElementById("custEmail").value.trim(),
+
+        address: document.getElementById("custAddress").value.trim()
+
+    };
+
+    if(!customer.name || !customer.mobile){
+
+        alert("Name and Mobile Required");
+
+        return;
+
+    }
+
+    const res = await fetch(BASE_URL + "/pos/customers",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify(customer)
+
+    });
+
+    const data = await res.json();
+
+    if(data.success){
+
+        alert("Customer Added Successfully");
+
+        closeCustomerPopup();
+
+        loadCustomers();
+
+    }else{
+
+        alert(data.message || "Failed");
+
+    }
+
+}
