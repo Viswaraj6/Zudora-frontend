340f2b5c040dbd8a222bfafca083f6a137ff9cd5
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-29 13:09:58 +0530

    Update billing.js

--- pos/js/billing.js
+++ pos/js/billing.js
@@ -1024,6 +1024,9 @@ if (gst !== "" && !/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.t
         // Close Dropdown
         document.getElementById("customerDropdown")
             .classList.add("hidden");
+        document
+    .getElementById("customerActions")
+    .classList.remove("hidden");
 
         // Clear Form
         document.getElementById("custName").value = "";
